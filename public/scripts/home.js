05a2f84a9f100484d07cdbd6e30e7d013c369e8e
Geoff Porter <geoff.porter@gmail.com>
2016-07-01 00:24:18 -0400

    Added contact info

---
+++ public/scripts/home.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  console.log('ready');
+  $('#resume-header-link').click(function() {
+    console.log('yup yup yup');
+     //$.scrollTo($('#resume-div'), 500);
+  });
+});
