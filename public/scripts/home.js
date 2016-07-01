03cbde0afd019dc8775362c0abbc4558612af167
Geoff Porter <geoff.porter@gmail.com>
2016-07-01 10:42:17 -0400

    Updated logo glyph

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-  console.log('ready');
-  $('#resume-header-link').click(function() {
-    console.log('yup yup yup');
-     //$.scrollTo($('#resume-div'), 500);
-  });
+  
 });
